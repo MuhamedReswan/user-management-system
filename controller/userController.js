9f0f105e3b19725d003574dbed39f3067ffaa2b9
MuhamedReswan <muhamedreswan9917@gmail.com>
2024-01-21 16:56:10 +0530

    updated

--- controller/userController.js
+++ controller/userController.js
@@ -9,7 +9,6 @@ const securedPassword = async (Password) => {
         return passwordHash;
     } catch (error) {
         console.log(error.message)
-
     }
 }
 
@@ -17,51 +16,20 @@ const securedPassword = async (Password) => {
 const loadRegister = async (req, res) => {
     try {
         res.render("registration")
-
     } catch (error) {
         console.log(error.message);
     }
-
 }
 
 
-
-
-// try to work on message
-
-// const insertUser = async (req, res) => {
-//     try {
-//         const sPassword = await securedPassword(req.body.Password);
-
-//         const user = new Users({
-//             name: req.body.Name,
-//             email: req.body.Email,
-//             mobile: req.body.Mobile,
-//             password: sPassword,
-           
-//             is_admin: 0,
-//         });
-
-//         const UserData = await user.save()
-//         if (UserData) {
-//             res.render('registration', { message: "your registration has been succesful..." })
-//         } else {
-//             res.render('registration', { message: "your registration has been failed..." })
-//         }
-//     } catch (error) {
-//         console.log(error);
-//     }
-
-// }
-
 // inset user
-const insertUser = async (req,res)=>{
+const insertUser = async (req, res) => {
     try {
 
         const email = req.body.Email;
         const namee = req.body.Name;
-        const finduser = await Users.findOne({email: email});
-        const findUserbyname = await Users.findOne({name: namee});
+        const finduser = await Users.findOne({ email: email });
+        const findUserbyname = await Users.findOne({ name: namee });
 
         if (finduser) {
             req.flash('exist', 'User already exists with this email');
@@ -72,8 +40,7 @@ const insertUser = async (req,res)=>{
             res.redirect('/register')
 
         }
-        else
-        {
+        else {
             const sPassword = await securedPassword(req.body.Password);
 
             const user = new Users({
@@ -91,11 +58,12 @@ const insertUser = async (req,res)=>{
                 res.render('registration', { message: "your registration has been failed..." })
             }
         }
-        
+
     } catch (error) {
         console.log(error)
     }
 }
+
 
 
 // login user method
@@ -120,7 +88,7 @@ const loginVerify = async (req, res) => {
             const passwordMatch = await bcrypt.compare(password, UserData.password)
             if (passwordMatch) {
                 req.session.user_id = UserData._id;
-                res.redirect("/home"); 
+                res.redirect("/home");
             }
         } else {
             res.render('login', { message: "incorrect email or password" })
@@ -134,15 +102,15 @@ const loginVerify = async (req, res) => {
 // user Home
 const loadHome = async (req, res) => {
     try {
-        const UserData = await Users.findById({_id:req.session.user_id}) 
-        res.render('home',{user:UserData});
+        const UserData = await Users.findById({ _id: req.session.user_id })
+        res.render('home', { user: UserData });
     } catch (error) {
         console.log(error);
     }
 };
 
 // user logout
-const userLogout= async(req,res)=>{
+const userLogout = async (req, res) => {
     try {
         req.session.destroy()
         res.redirect('/login')
@@ -152,29 +120,29 @@ const userLogout= async(req,res)=>{
 }
 
 // edit and upload
-const editLoad=async(req,res)=>{
-try {
+const editLoad = async (req, res) => {
+    try {
 
-    const id = req.query.id;
-    UserData= await Users.findById({_id:id})
-        res.render('edit',{user:UserData})
-       
-} catch (error) {
-    console.log(error);
-}
+        const id = req.query.id;
+        UserData = await Users.findById({ _id: id })
+        res.render('edit', { user: UserData })
+
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
 // update profile by user
-const updateProfile=async(req,res)=>{
+const updateProfile = async (req, res) => {
     try {
 
-        const UserData= await Users.findByIdAndUpdate({_id: req.params.id},{
-            name:req.body.name,
-            email:req.body.email,
-            mobile:req.body.mobile
+        const UserData = await Users.findByIdAndUpdate({ _id: req.params.id }, {
+            name: req.body.name,
+            email: req.body.email,
+            mobile: req.body.mobile
         })
-        console.log("userData"+UserData);
+        console.log("userData" + UserData);
         console.log(req.params.id);
         res.redirect('/home');
 
